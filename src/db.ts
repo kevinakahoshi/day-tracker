d46c45111d608245eb454e076128fbb7fdc46172
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2026-02-19 22:49:15 -0800

    Trying again

---
+++ src/db.ts
@@ -0,0 +1,11 @@
+import { neon } from '@neondatabase/serverless'
+
+let client: ReturnType<typeof neon>
+
+export async function getClient() {
+  if (!process.env.DATABASE_URL) return undefined
+
+  client ??= await neon(process.env.DATABASE_URL!)
+
+  return client
+}
